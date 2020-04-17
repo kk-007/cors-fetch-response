b746cbb78966a65b86ecbeba49979537c3b4be88
Keval navadiya <31183263+kk-007@users.noreply.github.com>
2020-04-17 15:25:10 +0530

    added GET request

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
+
+app.get('/',(req,res)=>{
+    if(req.query.url){
+        fetch(req.query.url)
+        .then(data=>data.json())
+        .then(result=>res.send(result));
+    }
+})
 
 app.listen(PORT,()=>console.log(`SERVER RUNNING ON PORT ${PORT}`));
